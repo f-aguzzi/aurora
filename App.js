eaa33d149b1a402bac66213cdbde38155da994a5
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-04-08 11:21:32 +0200

    Fixed issues with Expo

--- App.js
+++ App.js
@@ -3,26 +3,34 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CustomHeader from "./components/CustomHeader";
 import EntryMenu from './components/EntryMenu';
 import MainMenu from './components/MainMenu';
+import { Pages } from './types/types';
 
 
 export default function App() {
 
-  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(Pages.EntryMenu);
 
-  if (loading)
+  if (currentPage === Pages.EntryMenu)
     return (
       <SafeAreaProvider>
         <CustomHeader />
-        <EntryMenu setLoading={setLoading} />
+        <EntryMenu setCurrentPage={setCurrentPage} />
       </SafeAreaProvider>
     );
-  else
+  else if (currentPage === Pages.MainMenu) {
     return (
       <SafeAreaProvider>
         <CustomHeader />
-        <MainMenu />
+        <MainMenu setCurrentPage={setCurrentPage}/>
       </SafeAreaProvider>
     );
+  } else if (currentPage === Pages.Hives) {
+    return (
+      <SafeAreaProvider>
+        <CustomHeader />
+        <Hives />
+      </SafeAreaProvider>
+    )
+  }
 }
-
 
